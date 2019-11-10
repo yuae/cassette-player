aae7fbd451654a0167908f6005308cf4164026c7
YongYU <yoyu@upei.ca>
2019-11-10 16:44:57 -0400

    drawable file and ui update 1

--- GroupProject/app/src/main/java/com/example/groupproject/MainActivity.java
+++ GroupProject/app/src/main/java/com/example/groupproject/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void lastClick(View view)
+    {
+        ImageButton lastTrack_button = (ImageButton)findViewById(R.id.last_track_id);
+        ObjectAnimator last_button_anime =ObjectAnimator.ofFloat(lastTrack_button,"rotation",0, 360);
+        last_button_anime.setRepeatCount(Animation.INFINITE);
+
+        ImageButton next_track_button = (ImageButton)findViewById(R.id.next_track_id);
+        ObjectAnimator next_button_anime = ObjectAnimator.ofFloat(next_track_button,"rotation",0, 360);
+
+        next_button_anime.setRepeatCount(Animation.INFINITE);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(2000);
+        animatorSet.setInterpolator(new LinearInterpolator());
+        animatorSet.playTogether(last_button_anime, next_button_anime);
+        animatorSet.start();
+
+
+
+
+
+
+
+    }
+
+
+
+    public void pause_play(View view)
+    {
+
+    }
+
+
+    //  ObjectAnimator.ofFloat(lastTrack_button,"rotation",0, 45).start();
+
+
 }
+
+
